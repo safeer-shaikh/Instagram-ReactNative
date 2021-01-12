bcac414086d004aab5488355c9ea968a61785b62
safeer-shaikh <safeersohail210.ss@gmail.com>
2021-01-12 20:57:51 +0500

    search page created

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput  } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export default function Search() {
     return (
-        <View>
-            <Text>Search page</Text>
+        <View style={{backgroundColor: '#ffffff', flex: 1,}}>
+            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 20}}>
+                <MaterialCommunityIcons name='magnify' style={{fontSize: 28,}} />
+                <TextInput placeholder='Search' style={{backgroundColor: '#dbdbdb', width: '80%', marginLeft: 20, borderRadius: 20,}} />
+            </View>
         </View>
     )
 }
